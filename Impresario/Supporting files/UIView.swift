a02a29d07a741efa56c876cf8992e404f311cdf6
Arnaud Dalbin <arnaud@MacBook-Pro.local>
2021-05-09 12:38:58 +0200

    WIP sign in/up

---
+++ Impresario/Supporting files/UIView.swift
@@ -0,0 +1,29 @@
+//
+//  UIView.swift
+//  Impresario
+//
+//  Created by Arnaud Dalbin on 11/04/2021.
+//
+
+import UIKit
+
+extension UIView {
+    func setAnchorPoint(_ point: CGPoint) {
+        var newPoint = CGPoint(x: bounds.size.width * point.x, y: bounds.size.height * point.y)
+        var oldPoint = CGPoint(x: bounds.size.width * layer.anchorPoint.x, y: bounds.size.height * layer.anchorPoint.y);
+
+        newPoint = newPoint.applying(transform)
+        oldPoint = oldPoint.applying(transform)
+
+        var position = layer.position
+
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+
+        layer.position = position
+        layer.anchorPoint = point
+    }
+}
---
+++ Impresario/Views/ImpresarioApp.swift
@@ -0,0 +1,18 @@
+//
+//  ImpresarioApp.swift
+//  Impresario
+//
+//  Created by Arnaud Dalbin on 11/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ImpresarioApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
